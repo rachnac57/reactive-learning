fec0c8de7f9bf238bdbb39fbae349af3519803cb
rachna <kalpanac656@gmail.com>
2018-12-07 04:22:33 +0530

    Added forms to React app, with controlled components.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './App';
 import {Tick} from './Tick';	// named import
 import * as serviceWorker from './serviceWorker';
+import {Page} from './Conditional';
+import {NameForm} from './Forms';
 
 
 var x = document.getElementById('root');
 render(<App />, x);
 render(<Tick />, x);
+
+render(
+  <Page />,
+  document.getElementById('child')
+);
+
+render(
+	<NameForm/>, document.getElementById('sibling'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
