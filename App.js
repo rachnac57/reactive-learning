1f1db71adad09fb392ec7d151d055bb07c2146eb
Rachna <38106843+rachnac57@users.noreply.github.com>
2022-11-26 18:12:29 +0530

    Timer bosy

---
+++ App.js
@@ -0,0 +1,41 @@
+import "./styles.css";
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [start, setStart] = useState(false);
+
+  function startTimer() {
+    setStart(true);
+  }
+
+  function stopTimer() {
+    setStart(false);
+  }
+
+  const timer = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    let id;
+    if (start) {
+      id = setInterval(timer, 1000);
+    } else {
+      clearInterval(id);
+    }
+    return () => clearInterval(id);
+  }, [count, start]);
+
+  return (
+    <>
+      <strong>Count: </strong>
+      {count}
+      <br />
+      <button onClick={() => startTimer()}>Start</button>
+      <button onClick={() => stopTimer()}>Pause</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </>
+  );
+}
